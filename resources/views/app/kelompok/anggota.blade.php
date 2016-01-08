5c972aceb3542f51d1e0759392ed0a92c2b3b53c
lutfi <sainteclutfi@gmail.com>
2016-01-08 14:07:08 +0800

    anggotaview

---
+++ resources/views/app/kelompok/anggota.blade.php
@@ -0,0 +1,56 @@
+<table class="table table-hover demo-table-dynamic custom" id="tableWithDynamicRows">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Nama Anggota</th>
+								<th>Jabatan</th>
+								<th>Bantuan yang telah diterima</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>098901823</td>
+								<td>Gifa Eriyanto</td>
+								<td>Ketua</td>
+								<td>
+									<ul class="list-unstyled">
+										<li><b>2014:</b> Kapal/Perahu</li>
+										<li><b>2015:</b> Alat Tangkap</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>098901823</td>
+								<td>Gifa Eriyanto</td>
+								<td>Wakil Ketua</td>
+								<td>
+									<ul class="list-unstyled">
+										<li><b>2014:</b> Kapal/Perahu</li>
+										<li><b>2015:</b> Alat Tangkap</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>098901823</td>
+								<td>Gifa Eriyanto</td>
+								<td>Bendahara</td>
+								<td>
+									<ul class="list-unstyled">
+										<li><b>2014:</b> Kapal/Perahu</li>
+										<li><b>2015:</b> Alat Tangkap</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>098901823</td>
+								<td>Gifa Eriyanto</td>
+								<td>Anggota</td>
+								<td>
+									<ul class="list-unstyled">
+										<li><b>2014:</b> Kapal/Perahu</li>
+										<li><b>2015:</b> Alat Tangkap</li>
+									</ul>
+								</td>
+							</tr>
+						</tbody>
+					</table>
