765790286688128cc67478666e00b995f89a942a
GifaEriyanto <gifaeriyanto@ymail.com>
2016-01-23 08:30:17 +0800

    Pengolah

--- resources/views/app/pengolah/index.blade.php
+++ resources/views/app/pengolah/index.blade.php
@@ -1,9 +1,282 @@
 @extends('app.layout.main')
 
-@section('konten')
-konten
+@section('title')
+	Pembudidaya | Tambah
 @endsection
 
-@section('footer')
-footer
+
+
+@section('konten')
+
+<!-- START PAGE-CONTAINER -->
+<div class="page-container">
+
+	<!-- START PAGE CONTENT WRAPPER -->
+	<div class="page-content-wrapper">
+		
+		<!-- START PAGE CONTENT -->			
+		<div class="content">
+			
+			<div class="jumbotron bg-darkblue" data-pages="parallax">
+				<div class="container-fluid container-fixed-lg">
+					<div class="inner" style="transform: translateY(0px); opacity: 1;">
+						<!-- START BREADCRUMB -->
+						<ul class="breadcrumb pull-left">
+							<li>
+								<a href="{{ route('pembudidaya') }}">Pembudidaya</a>
+							</li>
+						</ul>
+						
+						<button id="show-tambah-pembudidaya" class="btn btn-primary bg-blueblur m-t-10 m-b-10 pull-right">Tambah</button>
+					</div>
+				</div>
+
+			</div>
+
+			<br>
+
+			<div class="container-fluid container-fixed-lg">
+				<div class="inner" style="transform: translateY(0px); opacity: 1;">
+
+					<div class="row">
+
+						@if ( Session::has('gagal') )
+				    		@include('app/layout/partials/alert-danger', ['message' => session('gagal')])
+						@endif
+
+						@if ( count($errors) > 0 )
+							@include('app/layout/partials/alert-danger', ['errors' => $errors])
+						@endif
+
+						<div id="tambah-pembudidaya" style="display:none">
+							<div class="col-lg-7 col-md-6 ">
+
+								<!-- START PANEL -->
+								<div class="panel panel-transparent">
+									<div class="panel-body">
+										<form id="form-personal" method="post" action="{{ route('pembudidaya_simpan') }}" role="form">
+											
+											{{ csrf_field() }}
+
+											<div class="row clearfix">
+												<div class="col-sm-6">
+													<div class="form-group required">
+														<label>NIK</label>
+														<input type="text" class="form-control number" name="nik" value="{{ Input::old('nik') }}" required>
+													</div>
+												</div>
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label>Nama Lengkap</label>
+														<input type="text" class="form-control" name="name" name="nik" value="{{ Input::old('name') }}" required>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-sm-12">
+													<div class="form-group">
+														<label>Alamat</label>
+														<input type="text" class="form-control" name="alamat" value="{{ Input::old('alamat') }}" required>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-md-12">
+													<div class="form-group">
+														<label>Jenis Usaha Budidaya</label>
+														<select onchange="get_usaha(this.value)" class="full-width" name="jenis_usaha" required data-init-plugin="select2">
+															<option value="">Pilih Jenis Usaha...</option>
+															<option value="Budidaya Air Laut" {{ Input::old('jenis_usaha') == "Budidaya Air Laut" ? "selected":"" }}>Budidaya Air Laut</option>
+															<option value="Budidaya Air Tawar" {{ Input::old('jenis_usaha') == "Budidaya Air Tawar" ? "selected":"" }}>Budidaya Air Tawar</option>
+															<option value="Budidaya Air Payau" {{ Input::old('jenis_usaha') == "Budidaya Air Payau" ? "selected":"" }}>Budidaya Air Payau</option>
+														</select>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<div class="col-md-6">
+													<div class="form-group">
+														<label>Jenis Olahan</label>
+														<div id="olahan">
+															<select class="full-width" data-init-plugin="select2" disabled>
+																<option value="">Pilih Jenis Olahan...</option>
+															</select>
+														</div>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="form-group">
+														<label>Kepemilikan Sarana dan Prasarana</label>
+														<div id="sarana">
+															<select class="full-width" data-init-plugin="select2" disabled>
+																<option value="">Pilih Sarana / Prasarana...</option>
+															</select>
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<div class="clearfix"></div>
+											<br>
+											
+											<button class="btn btn-primary" type="submit">Tambah</button>
+										</form>
+									</div>
+								</div>
+								<!-- END PANEL -->
+							</div>
+						</div>
+
+						<div class="col-md-12">
+								
+								@if ( Session::has('success') ) 
+						    		@include('app/layout/partials/alert-sukses', ['message' => session('success')])
+								@endif
+							
+							<!-- START PANEL -->
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<div class="">
+										<div class="input-group">
+											<input type="text" onkeyup="cari_data(this.value)" class="form-control" placeholder="Pencarian">
+											<span class="input-group-btn">
+												<a href="" class="btn btn-default" data-toggle="modal" data-target="#modal-ekspor"><i class="fa fa-file-archive-o"></i> &nbsp;Ekspor</a>
+											</span>
+										</div>
+										<br>
+
+										<div id="show-pencarian"></div>
+
+										<div id="show-data">
+											<table class="table table-hover demo-table-dynamic custom" id="tableWithDynamicRows">
+												<thead>
+													<tr>
+														<th>
+															<button class="btn btn-check" data-toggle="modal" data-target="#modal-hapus" disabled id="hapus"><i class="pg-trash"></i></button>
+														</th>
+														<th>Nama Lengkap</th>
+														<th>Nama Kelompok</th>
+														<th>Jabatan Kelompok</th>
+														<th>Jenis Olahan</th>
+														<th style="text-align:center">Aksi</th>
+													</tr>
+												</thead>
+
+												<tbody>
+												</tbody>
+
+											</table>
+										</div>
+
+									</div>
+								</div>
+							</div>
+							<!-- END PANEL -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END PAGE CONTENT -->
+		<!-- START COPYRIGHT -->
+			@include('app.layout.partials.copyright')
+		<!-- END COPYRIGHT -->
+
+	</div>
+	<!-- END PAGE CONTENT WRAPPER -->
+
+</div>
+<!-- END PAGE CONTAINER -->
+
+<!-- MODAL STICK UP VIEW -->
+<div class="modal fade stick-up" id="modal-view" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content-wrapper">
+			<div class="modal-content">
+				<div class="modal-header clearfix text-left">
+					<button type="button" class="close" data-dismiss="modal"  aria-hidden="true"><i class="pg-close fs-14"></i></button>
+					<h5>Detail Pembudidaya</h5>
+				</div>
+				<div class="modal-body" id="view-detail">
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default btn-cons no-margin inline" data-dismiss="modal">Kembali</button>
+				</div>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+
+<!-- MODAL STICK UP SMALL ALERT -->
+<div class="modal fade stick-up" id="modal-hapus" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content-wrapper">
+			<div class="modal-content">
+				<div class="modal-header clearfix text-left">
+					<button type="button" class="close" data-dismiss="modal"  aria-hidden="true"><i class="pg-close fs-14"></i></button>
+					<h5>Hapus Data</h5>
+				</div>
+				<div class="modal-body">
+					<p class="no-margin">Data akan dihapus. Apakah Anda yakin?</p>
+				</div>
+				<div class="modal-footer">
+					<a class="btn btn-danger btn-hapus btn-cons pull-left inline">Ya</a>
+					<button type="button" class="btn btn-default btn-cons no-margin pull-left inline" data-dismiss="modal">Tidak</button>
+				</div>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- END MODAL STICK UP SMALL ALERT -->
+
+
+<!-- MODAL STICK UP SMALL ALERT -->
+<div class="modal fade slide-up" id="modal-ekspor" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content-wrapper">
+			<div class="modal-content">
+				<div class="modal-header clearfix text-left">
+					<button type="button" class="close" data-dismiss="modal"  aria-hidden="true"><i class="pg-close fs-14"></i></button>
+					<h5>Ekspor Data</h5>
+					<hr>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-md-6">
+							<a href="{{ url('/app/pembudidaya/export-excel') }}">
+								<i class="fa fa-file-excel-o export-excel"></i>
+								Unduh Dalam Format Mic.Excel
+							</a>
+						</div>
+						<div class="col-md-6">
+							<a href="{{ url('/app/pembudidaya/export-pdf') }}">
+								<i class="fa fa-file-pdf-o export-pdf"></i>
+								Unduh Dalam Format PDF
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- END MODAL STICK UP SMALL ALERT -->
+
+@endsection
+
+
+@section('registerscript')
+	<script>
+		$(".menu-items .link-pengolah").addClass("active");
+	</script>
 @endsection
