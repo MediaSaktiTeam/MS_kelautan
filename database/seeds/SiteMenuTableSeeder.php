4a193718e224bd8f010c93c0b38668bf9345115a
Rahmat <abd.rahmat.ika@gmail.com>
2016-05-22 18:03:17 -0800

    -

--- database/seeds/SiteMenuTableSeeder.php
+++ database/seeds/SiteMenuTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class SiteMenuTableSeeder extends Seeder
 {
@@ -12,103 +13,102 @@ class SiteMenuTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
-        \DB::table('site_menu')->delete();
-        
-        \DB::table('site_menu')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'judul' => 'Beranda',
-                'link' => '/',
-                'parent_id' => 0,
-                'urutan' => 1,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'judul' => 'Profil',
-                'link' => '#',
-                'parent_id' => 0,
-                'urutan' => 2,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'judul' => 'Visi, Misi, Kebijakan Pembangunan',
-                'link' => 'http://kelautan.dev/page/visi-misi',
-                'parent_id' => 2,
-                'urutan' => 3,
-                'lokasi' => 'Header',
-                'jenis' => 'Halaman',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'judul' => 'Peta Kab. Bantaeng',
-                'link' => 'http://kelautan.dev/page/peta-kab-bantaeng',
-                'parent_id' => 2,
-                'urutan' => 1,
-                'lokasi' => 'Header',
-                'jenis' => 'Halaman',
-            ),
-            4 => 
-            array (
-                'id' => 5,
-                'judul' => 'Struktur Organisasi',
-                'link' => 'http://kelautan.dev/page/struktur-organisasi',
-                'parent_id' => 2,
-                'urutan' => 2,
-                'lokasi' => 'Header',
-                'jenis' => 'Halaman',
-            ),
-            5 => 
-            array (
-                'id' => 6,
-                'judul' => 'Data Umum',
-                'link' => '#',
-                'parent_id' => 0,
-                'urutan' => 3,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-            6 => 
-            array (
-                'id' => 7,
-                'judul' => 'Bidang',
-                'link' => '#',
-                'parent_id' => 0,
-                'urutan' => 4,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-            7 => 
-            array (
-                'id' => 8,
-                'judul' => 'Sekretariat',
-                'link' => '#',
-                'parent_id' => 0,
-                'urutan' => 5,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-            8 => 
-            array (
-                'id' => 9,
-                'judul' => 'Galeri',
-                'link' => '#',
-                'parent_id' => 0,
-                'urutan' => 6,
-                'lokasi' => 'Header',
-                'jenis' => 'Custom',
-            ),
-        ));
-        
+    
+    App\Menu::truncate();
+    
+    \DB::table('site_menu')->insert(array (
+        0 => 
+        array (
+            'id' => 1,
+            'judul' => 'Beranda',
+            'link' => '/',
+            'parent_id' => 0,
+            'urutan' => 1,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+        1 => 
+        array (
+            'id' => 2,
+            'judul' => 'Profil',
+            'link' => '#',
+            'parent_id' => 0,
+            'urutan' => 2,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+        2 => 
+        array (
+            'id' => 3,
+            'judul' => 'Visi, Misi, Kebijakan Pembangunan',
+            'link' => 'http://kelautan.dev/page/visi-misi',
+            'parent_id' => 2,
+            'urutan' => 3,
+            'lokasi' => 'Header',
+            'jenis' => 'Halaman',
+        ),
+        3 => 
+        array (
+            'id' => 4,
+            'judul' => 'Peta Kab. Bantaeng',
+            'link' => 'http://kelautan.dev/page/peta-kab-bantaeng',
+            'parent_id' => 2,
+            'urutan' => 1,
+            'lokasi' => 'Header',
+            'jenis' => 'Halaman',
+        ),
+        4 => 
+        array (
+            'id' => 5,
+            'judul' => 'Struktur Organisasi',
+            'link' => 'http://kelautan.dev/page/struktur-organisasi',
+            'parent_id' => 2,
+            'urutan' => 2,
+            'lokasi' => 'Header',
+            'jenis' => 'Halaman',
+        ),
+        5 => 
+        array (
+            'id' => 6,
+            'judul' => 'Data Umum',
+            'link' => '#',
+            'parent_id' => 0,
+            'urutan' => 3,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+        6 => 
+        array (
+            'id' => 7,
+            'judul' => 'Bidang',
+            'link' => '#',
+            'parent_id' => 0,
+            'urutan' => 4,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+        7 => 
+        array (
+            'id' => 8,
+            'judul' => 'Sekretariat',
+            'link' => '#',
+            'parent_id' => 0,
+            'urutan' => 5,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+        8 => 
+        array (
+            'id' => 9,
+            'judul' => 'Galeri',
+            'link' => '#',
+            'parent_id' => 0,
+            'urutan' => 6,
+            'lokasi' => 'Header',
+            'jenis' => 'Custom',
+        ),
+    ));
         
     }
+
 }
