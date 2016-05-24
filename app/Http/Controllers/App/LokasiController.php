881d8f62c60a28de65bad653a5581dd9265d4aa3
Rahmat <abd.rahmat.ika@gmail.com>
2016-05-23 17:47:49 -0800

    -

--- app/Http/Controllers/App/LokasiController.php
+++ app/Http/Controllers/App/LokasiController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use DB,Excel,PDF;
-use App\User,App\Provinsi,App\Kabupaten,App\Kecamatan,App\Desa;
-use App\Permissions;
 
 class LokasiController extends Controller
 {
